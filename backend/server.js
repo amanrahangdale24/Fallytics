4fafef8a4c5feccf055810ffae627d268afd28f6
Aman Rahangdale <147967516+amanrahangdale24@users.noreply.github.com>
2026-02-07 11:56:08 +0530

    make the backend production ready

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,21 @@ connectToDB();
 
 app.use(express.json()); 
 app.use(cookieParser());
-app.use("/api/auth", authRoutes); 
+app.use("/api/auth", authRoutes);
+
+
+// make ready for deployment 
+// setting in a way, where when we run the backend, our frontend will run auto inside the backend. 
+
+if (ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    });
+    // any route other than the defined above, will hit the *; 
+}
+
 
 app.listen(process.env.PORT || 4044, ()=>{
     console.log("Server is listening on PORT 4044"); 
