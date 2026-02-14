9338dc912913073dd31af8f3a575093334127680
Aman Rahangdale <147967516+amanrahangdale24@users.noreply.github.com>
2026-02-14 12:03:14 +0530

    Implement logout route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -89,3 +89,8 @@ export const login = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const logout = async(_,res)=>{
+    res.cookie("jwt", "", {maxAge:0});
+    res.status(200).json({message:"Logged out successfully"}); 
+}
